9e3ccdf63461491420a98e79af9eff74374d8415
EstebanArchbold <est.archbold.edas@gmail.com>
2024-03-16 23:15:26 -0400

    fix

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const hello_js_1 = require("./hello.js");
-const users_data_1 = require("./users.data");
-(0, users_data_1.getData)()
-    .then((data) => {
-    (0, hello_js_1.sayHello)();
-    console.log(data);
-    (0, hello_js_1.sayGoodBye)();
-})
-    .catch((err) => {
-    console.error("ERROR: User data not returned");
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const mime_types_1 = __importDefault(require("mime-types"));
+let lookup = mime_types_1.default.lookup;
+const port = process.env.PORT || 3000;
+const server = http_1.default.createServer((req, res) => {
+    let path = req.url;
+    if (path === "/" || path === "/home") {
+        path = "index.html";
+    }
+    let mime_type = lookup(path.substring(1));
+    fs_1.default.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end("Error 404 - File Not Found " + err.message);
+            return;
+        }
+        if (!mime_type) {
+            mime_type = "text/plain";
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200, { 'Content-Type': mime_type });
+        res.end(data);
+    });
+});
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
 //# sourceMappingURL=server.js.map
